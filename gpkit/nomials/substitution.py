b41cd819992658232e2b66bf716662f0cea26669
whoburg <whoburg@alum.mit.edu>
2025-05-17 12:24:52 -0500

    fix failing unit test - ragged substitution

--- gpkit/nomials/substitution.py
+++ gpkit/nomials/substitution.py
@@ -45,7 +45,7 @@ def append_sub(sub, keys, constants, sweep, linkedsweep):
                 pywarnings.filterwarnings("error")
                 try:
                     sub = np.array(sub) if not hasattr(sub, "shape") else sub
-                except Warning:  # pragma: no cover  #TODO: coverage this
+                except ValueError:  # pragma: no cover  #TODO: coverage this
                     # ragged nested sequences, eg [[2]], [3, 4]], in py3.7+
                     sub = np.array(sub, dtype=object)
             if key.shape == sub.shape:
